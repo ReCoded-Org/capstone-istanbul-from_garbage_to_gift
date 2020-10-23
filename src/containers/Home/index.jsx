6decdcd95e0374b4f221694906dcfe67cf224b55
Orçun Sağırsoy <50768273+orcunsagirsoy@users.noreply.github.com>
2020-10-23 23:52:38 +0300

    Setup (#9)
    * test
    
    * setup completed
    
    * fixed the setup
    
    * index.css added, imports removed
    
    * yarn.lock deleted
    
    * "revert yarn.lock"
    Revert "yarn.lock deleted"
    
    This reverts commit a8907c38e199736612ee7b8257d0740997fe0cf7.
    
    * width of howitworks container changed
    
    * width changed
    
    * index.css relocated

---
+++ src/containers/Home/index.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+import HowItWorks from "../../components/HowItWorks";
+
+export default function Home() {
+  return (
+    <div>
+      <h1>This is Home Page!</h1>
+      <HowItWorks />
+    </div>
+  );
+}
