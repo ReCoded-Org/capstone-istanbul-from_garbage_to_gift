408da9fb03c7ff6977397ae3ba25eccd756aef18
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-11-10 19:31:01 +0300

    delete i18 provider and font size to em

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,13 @@
 import React from "react";
 import Home from "./containers/Home";
 import Footer from "./components/Footer";
-import i18n from "./i18n";
-import { I18nextProvider } from "react-i18next";
 
 function App() {
   return (
-    <I18nextProvider i18n={i18n}>
+    <>
       <Home />
       <Footer />
-    </I18nextProvider>
+    </>
   );
 }
 
