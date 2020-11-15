9bf3419ea2ff0fa11818f640468802e6b7884716
Betul Erkoc <betulfatmaerkoc@gmail.com>
2020-11-15 22:54:23 +0300

    Feat: start chat component

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import HowItWorks from "../../components/HowItWorks";
+import Chat from "../../Chat"
 
 export default function Home() {
   return (
     <div>
       <h1>This is Home Page!</h1>
       <HowItWorks />
+      <Chat/>
     </div>
   );
 }
