674d266d55156001ea4b8e0104395a8efb74fc86
Luey Monla Ahmed <67810739+Luey-A@users.noreply.github.com>
2020-11-15 15:34:10 +0300

    1st task project home (#22)
    * Feat: add project description to the home page
    
    add project description because there was none
    
    * Feat: update component and classnames and removed one unnecessary div from the img tag
    
    * Feat: merge dev branch with project description branch, commit project description branch after fixing component name classnames and removing an unecessary div from the img tag
    
    * Feat: delete package-lock.json, fix howItWorks calssName, delete img css height style, remove duplicate styles for the h1 and p in the homepage and add a class for different styles, merge the imports of container row and col into one import in index.js project description, delete repeated import of bootstrap from the index.jsx app
    
    * Feat: create new branch called 1stTaskProjectHome, change all the pxs in the css to em
    
    * Feat: close #4 node.js CI test fix
    
    * Feat: delete the line-height from ProjectDescription component index.css because it is not needed for the style of the component
    
    * Feat: delete line-height style from ProjectDescription component index.css because it was not needed
    
    Co-authored-by: Ammar Sammour <35445761+Ammar-64@users.noreply.github.com>

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,9 +1,11 @@
 import React from "react";
+import ProjectDescription from "../../components/ProjectDescription";
 import SuccessStories from "../../components/SuccessStories";
 
 export default function Home() {
   return (
     <div className="container">
+      <ProjectDescription />
       <SuccessStories />
     </div>
   );
