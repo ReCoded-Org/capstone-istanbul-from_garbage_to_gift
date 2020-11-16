3c2513544e65b64fa49a6b82c8d0a52c17a8d8f8
Knar Gocer <67804718+knargocer@users.noreply.github.com>
2020-11-16 15:20:32 +0300

    Feat:add whoweare component (#13)
    * Feat:add whoweare component
    
    * Sytle: fix the formatting and video link
    
    * Test: fix the test
    
    * Style: Fix according to the feedbacks
    
    * Style:Change the px to em
    
    * fix: doubled WhoWeAre componenet
    
    * Style: Give the necessary class names
    
    * Test:Correct the test
    
    * Style: Change class names to camel case
    
    * Style: changed the class name

--- src/containers/Home/index.jsx
+++ src/containers/Home/index.jsx
@@ -1,11 +1,13 @@
 import React from "react";
 import ProjectDescription from "../../components/ProjectDescription";
 import SuccessStories from "../../components/SuccessStories";
+import WhoWeAre from "../../components/WhoWeAre";
 
 export default function Home() {
   return (
     <div className="container">
       <ProjectDescription />
+      <WhoWeAre />
       <SuccessStories />
     </div>
   );
