2432e027ddfd427958435d800380e638608ffd0d
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-11-21 19:28:37 +0300

    refactor upload image function

--- src/components/Profile/BasicInfoSection/index.jsx
+++ src/components/Profile/BasicInfoSection/index.jsx
@@ -1,61 +1,38 @@
 import React, { useState } from "react";
 import { storage } from "../../../firebaseConfig";
+import { useTranslation } from "react-i18next";
 
 export default function BasicInfo({ data }) {
-  console.log(data);
+  const { t } = useTranslation();
+  const [file, setFile] = useState(null);
+  const [url, setURL] = useState("");
 
-  const allInputs = { imgUrl: "" };
-  const [imageAsFile, setImageAsFile] = useState("");
-  const [imageAsUrl, setImageAsUrl] = useState(allInputs);
+  function handleChange(e) {
+    setFile(e.target.files[0]);
+  }
 
-  const handleImageAsFile = (e) => {
-    const image = e.target.files[0];
-    setImageAsFile((imageFile) => image);
-  };
-
-  const handleFireBaseUpload = (e) => {
+  function handleUpload(e) {
     e.preventDefault();
-    console.log("start of upload");
-    if (imageAsFile === "") {
-      console.error(`not an image, the image file is a ${typeof imageAsFile}`);
-    }
-    const uploadTask = storage
-      .ref(`/images/${imageAsFile.name}`)
-      .put(imageAsFile);
-
-    uploadTask.on(
-      "state_changed",
-      (snapShot) => {
-   
-        console.log(snapShot);
-      },
-      (err) => {
-       
-        console.log(err);
-      },
-      () => {
-
-        storage
-          .ref("images")
-          .child(imageAsFile.name)
-          .getDownloadURL()
-          .then((fireBaseUrl) => {
-            setImageAsUrl((prevObject) => ({
-              ...prevObject,
-              imgUrl: fireBaseUrl,
-            }));
-          });
-      }
-    );
-  };
+    const uploadTask = storage.ref(`/images/${file.name}`).put(file);
+    uploadTask.on("state_changed", console.log, console.error, () => {
+      storage
+        .ref("images")
+        .child(file.name)
+        .getDownloadURL()
+        .then((url) => {
+          setFile(null);
+          setURL(url);
+        });
+    });
+  }
 
   return (
     <div>
-      <form onSubmit={handleFireBaseUpload}>
-        <input type="file" onChange={handleImageAsFile} />
-        <button>upload to firebase</button>
+      <form onSubmit={handleUpload}>
+        <input type="file" onChange={handleChange} />
+      <button>{t('profilePage.basicInfo.uploadBtn')}</button>
       </form>
-      <img src={imageAsUrl.imgUrl} alt="image tag" />
+      <img src={url} alt="profile" />
       <h5>{data.imageUrl}</h5>
       <h6>{data.name}</h6>
       <p>{data.description}</p>
