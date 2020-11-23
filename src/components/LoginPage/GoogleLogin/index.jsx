295ce7ca42bf31f79ed2372912b44b3976fde820
Betül Erkoç <40780338+betulerkoc@users.noreply.github.com>
2020-11-24 00:51:31 +0300

    Bug bash/login (#74)
    * Fix: fix conflict
    
    * Fix: fix sign up
    
    * Fix: fix chat styling
    
    * Fix: fix styling
    
    * Fix: fix chat
    
    * Fix: delete unnecessary component
    
    * Fix: update tests
    
    Co-authored-by: Ammar Sammour <35445761+Ammar-64@users.noreply.github.com>

--- src/components/LoginPage/GoogleLogin/index.jsx
+++ src/components/LoginPage/GoogleLogin/index.jsx
@@ -2,35 +2,24 @@
 // lg85eby3A2b9TtGrOq4BSwao
 
 import React from "react";
-import { useGoogleLogin } from "react-google-login";
-import { refreshTokenSetup } from "../utils/refreshToken";
 import googleIcon from "./icons/google.svg";
 import "./index.css";
 import { useTranslation } from "react-i18next";
+import firebase from "firebase/app";
+import "firebase/auth";
 
-const clientId =
-  "743068888490-2chsv38s7paclkbc1qjd2ktd3jjf2aqf.apps.googleusercontent.com";
+const auth = firebase.auth();
+
+const signInWithGoogle = () => {
+  const provider = new firebase.auth.GoogleAuthProvider();
+  auth.signInWithPopup(provider);
+};
 
 function Login() {
   const { t } = useTranslation();
-  const onSuccess = (res) => {
-    refreshTokenSetup(res);
-  };
-
-  const onFailure = (res) => {
-    alert(t("loginPages.login.alertFailLogin"));
-  };
-
-  const { signIn } = useGoogleLogin({
-    onSuccess,
-    onFailure,
-    clientId,
-    isSignedIn: true,
-    accessType: "offline",
-  });
 
   return (
-    <button onClick={signIn} className="buttonGoogle">
+    <button onClick={signInWithGoogle} className="buttonGoogle">
       <div>
         <img
           src={googleIcon}
