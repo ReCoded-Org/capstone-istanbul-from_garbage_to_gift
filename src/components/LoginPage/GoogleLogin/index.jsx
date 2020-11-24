c6cbc88c1a359af75a5e1106a626f4f314a67ed7
Luey Monla Ahmed <67810739+Luey-A@users.noreply.github.com>
2020-11-24 12:01:24 +0300

    Google sign in fix (#83)
    * Feat: add project description to the home page
    
    add project description because there was none
    
    * Feat: update component and classnames and removed one unnecessary div from the img tag
    
    * Feat: merge dev branch with project description branch, commit project description branch after fixing component name classnames and removing an unecessary div from the img tag
    
    * Feat: delete package-lock.json, fix howItWorks calssName, delete img css height style, remove duplicate styles for the h1 and p in the homepage and add a class for different styles, merge the imports of container row and col into one import in index.js project description, delete repeated import of bootstrap from the index.jsx app
    
    * Feat: create new branch called 1stTaskProjectHome, change all the pxs in the css to em
    
    * Feat: close #4 node.js CI test fix
    
    * fix application post
    
    * remove console.log
    
    * fix google sign in
    
    * fix google sign in
    
    * fix google sign in
    
    Co-authored-by: Ammar Sammour <35445761+Ammar-64@users.noreply.github.com>

--- src/components/LoginPage/GoogleLogin/index.jsx
+++ src/components/LoginPage/GoogleLogin/index.jsx
@@ -2,37 +2,23 @@
 // lg85eby3A2b9TtGrOq4BSwao
 
 import React from "react";
-import { useGoogleLogin } from "react-google-login";
 import googleIcon from "./icons/google.svg";
 import "./index.css";
 import { useTranslation } from "react-i18next";
 import "firebase/auth";
-import { refreshTokenSetup } from "../utils/refreshToken";
+import { auth } from "../../../firebaseConfig";
+import firebase from "firebase";
 
 function Login() {
   const { t } = useTranslation();
-
-  const clientId =
-    "743068888490-2chsv38s7paclkbc1qjd2ktd3jjf2aqf.apps.googleusercontent.com";
-  const onSuccess = (res) => {
-    refreshTokenSetup(res);
+  const loginWithGoogle = async () => {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    provider.addScope("https://www.googleapis.com/auth/contacts.readonly");
+    await auth.signInWithPopup(provider);
   };
-
-  const onFailure = (res, e) => {
-    e.preventDefault();
-    alert(t("loginPages.login.alertFailLogin"));
-  };
-
-  const { signIn } = useGoogleLogin({
-    onSuccess,
-    onFailure,
-    clientId,
-    isSignedIn: true,
-    accessType: "offline",
-  });
 
   return (
-    <button onClick={signIn} className="buttonGoogle">
+    <button onClick={loginWithGoogle} className="buttonGoogle">
       <div>
         <img
           src={googleIcon}
