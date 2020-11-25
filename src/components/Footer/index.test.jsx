6648898dfe80af288b8d534c83976d8b1a060ea6
orcunsagirsoy <orcunsagirsoy@gmail.com>
2020-11-25 04:38:02 +0300

    delete test footer

--- src/components/Footer/index.test.jsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import renderer from "react-test-renderer";
-import Footer from "./index";
-
-it("renders Footer correctly", () => {
-  const tree = renderer.create(<Footer />).toJSON();
-  expect(tree).toMatchSnapshot();
-});
